5c84207f8f575a6800e74ff990d2c96b1fe10c8a
Gumatan <arnaut.demaret@gmail.com>
2019-12-17 18:05:39 +0100

    updated the way the puzzle solution is defined and handled to match backend needs

--- frontend/src/components/Game/TopHints.jsx
+++ frontend/src/components/Game/TopHints.jsx
@@ -1,67 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class TopHints extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      topHints: null
-    };
-  }
+const TopHints = ({ solutionString, gameHeight, gameWidth }) => {
+  const [topHints, topHintsUpdate] = useState(null);
+  console.log(solutionString, gameHeight, gameWidth);
 
-  determineHints() {
-    let topHints = new Array(this.props.gameWidth)
+  const determineHints = () => {
+    let hints = new Array(gameWidth)
       .fill(0)
-      .map(() => new Array(Math.ceil(this.props.gameHeight / 2)).fill(0)); //Custom double entry array to the size of the game
+      .map(() => new Array(Math.ceil(gameHeight / 2)).fill(0)); //Custom double entry array to the size of the game
     let currentHintIndex;
     let currentValue;
 
-    for (let i = 0; i < this.props.gameHeight; i++) {
+    for (let i = 0; i < gameHeight; i++) {
       //For each row
       currentHintIndex = 0;
       currentValue = 1;
 
-      for (let j = 0; j < this.props.gameWidth; j++) {
+      for (let j = 0; j < gameWidth; j++) {
         //For each cell in row
 
-        if (this.props.solutionTab[j][i] === 1) {
+        if (solutionString[j * 10 + i] === "1") {
           //If current Cell == 1
-          topHints[i][currentHintIndex] = currentValue; //Increment related Hint
+          hints[i][currentHintIndex] = currentValue; //Increment related Hint
           currentValue += 1; //^
 
           if (
-            this.props.solutionTab[j + 1] &&
-            this.props.solutionTab[j + 1][i] === 0
+            solutionString[j * 10 + 10] &&
+            solutionString[j * 10 + 10 + i] === "0"
           ) {
             //If next cell in row exists and is equal to 0
-
             currentHintIndex += 1; //Start a new hint for the row
             currentValue = 1; //And reset it's value
           }
         }
       }
     }
-    topHints = topHints.map(item => item.filter(values => values !== 0)); //removing 0's in hints
-    this.setState({ topHints: topHints });
-  }
+    hints = hints.map(item => item.filter(values => values !== 0)); //removing 0's in hints
+    console.log(hints);
+    topHintsUpdate(hints);
+  };
 
-  componentDidMount() {
-    this.determineHints();
-  }
+  useEffect(() => {
+    determineHints();
+  }, []);
 
-  render() {
-    return (
-      <div className="TopHints">
-        {this.state.topHints &&
-          this.state.topHints.map(e => (
-            <div>
-              {e.map(n => (
-                <div>{n}</div>
-              ))}
-            </div>
-          ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="TopHints">
+      {topHints &&
+        topHints.map(e => (
+          <div>
+            {e.map(n => (
+              <div>{n}</div>
+            ))}
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default TopHints;
--- frontend/src/reducer.js
+++ frontend/src/reducer.js
@@ -3,122 +3,13 @@ const initialState = {
     id: 1,
     name: "Saucisson",
     creator: "Gum",
-    size: { x: 10, y: 10 },
-    solutionTab: [
-      [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-      [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-      [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
-      [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-      [0, 0, 1, 0, 0, 1, 1, 0, 0, 0],
-      [0, 1, 1, 1, 1, 1, 0, 1, 0, 0],
-      [0, 1, 1, 0, 1, 1, 1, 1, 0, 0],
-      [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-      [1, 0, 0, 1, 0, 1, 0, 0, 1, 0]
-    ]
+    height: 10,
+    width: 10,
+    solutionString:
+      "0000111000000101000000010100000010001000001001100001111101000110111100111111111110001000101001010010"
   },
-  tilesState: [
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0
-  ]
+  tilesState:
+    "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 };
 
 const reducer = (state = initialState, action) => {
@@ -129,11 +20,19 @@ const reducer = (state = initialState, action) => {
         currentPuzzleData: action.puzzleData
       };
     case "UPDATE_TILE":
-      const { tilesState } = state;
-      tilesState[action.id] = action.newState;
+      let { tilesState } = state;
+      tilesState =
+        tilesState.substr(0, action.id) +
+        action.newStatus +
+        tilesState.substr(action.id + 1);
       return {
         ...state,
         tilesState
+      };
+    case "RESET_TILES_STATUS":
+      return {
+        ...state,
+        tilesState: initialState.tilesState
       };
     default:
       return state;
